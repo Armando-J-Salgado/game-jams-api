b9ff471265a9b8b0824e5097e0a0009fcffd0366
Nehemías Rivas <167656622+NehemiasRivas7@users.noreply.github.com>
2026-03-30 07:48:31 -0600

    Feat: add token-based authentication with Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,13 +14,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Team;
 use App\Models\Competition;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,44 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Routes that do not require Authentication
+Route::post('v1/auth/login', [AuthController::class, "login"]);
+
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function() {
+
+    // Routes that require Authentication
+    // Authtentication routes
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::get('/auth/profile', [AuthController::class, 'profile']);
+
+    // CRUD users
+
+
+
+    // CRUD Categories
+
+    
+
+    // CRUD Handovers
+
+
+
+    // CRUD Utils
+
+    
+
+    // CRUD Teams
+
+
+
+    // CRUD Competitions
+
+    
+
+    // CRUD Modules
+});
+
