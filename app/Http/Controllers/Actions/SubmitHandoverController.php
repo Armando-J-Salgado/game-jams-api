05d256a4bf09be862dc33166137fa965b4e8c8e6
alxjr.wav <20245387@esen.edu.sv>
2026-04-02 23:39:54 -0600

    chore: utils __invoke controllers Added all the controllers needed for the following actions: - Add & Remove Team member - Enroll & Withdraw Team - Submit Handover + FormRequest

---
+++ app/Http/Controllers/Actions/SubmitHandoverController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Actions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Handover;
+use App\Http\Requests\SubmitHandoverRequest;
+
+class SubmitHandoverController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(SubmitHandoverRequest $request, Handover $handover)
+    {
+        // Enforce policy via Gate (HandoverPolicy already verifies if user belongs to the associated team)
+        Gate::authorize('update', $handover);
+
+        $handover->update([
+            'attachment' => $request->attachment,
+            'is_delivered' => true,
+            'date_of_submission' => now(),
+        ]);
+
+        return response()->json([
+            'message' => 'Handover submitted successfully',
+            'handover' => $handover,
+        ]);
+    }
+}
